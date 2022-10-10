970825feabd36196906caead762500231e98ad42
v4d1kos <v4dikos@yandex.ru>
2022-10-10 15:53:42 +0700

    db config

--- server/index.js
+++ server/index.js
@@ -1,9 +1,24 @@
 require('dotenv').config();
 const express = require('express');
+const sequelize = require('./db');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-});
+
+const start = async () => {
+    try{
+        await sequelize.authenticate();
+        await sequelize.sync();
+
+        app.listen(PORT, () => {
+            console.log(`Server started on port ${PORT}`);
+        });
+
+    }
+    catch (eroor){
+        console.log(eroor);
+    }
+}
+
+start();
